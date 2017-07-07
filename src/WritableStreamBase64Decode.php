810fab0c5606cd61ac955ba6993f8a3349162cd7
Cees-Jan Kiewiet <ceesjank@gmail.com>
2017-07-08 00:53:54 +0200

    Writable stream base64 decoding

---
+++ src/WritableStreamBase64Decode.php
@@ -0,0 +1,65 @@
+<?php declare(strict_types=1);
+
+namespace WyriHaximus\React\Stream\Base64;
+
+use Evenement\EventEmitterTrait;
+use React\Stream\WritableStreamInterface;
+
+final class WritableStreamBase64Decode implements WritableStreamInterface
+{
+    use EventEmitterTrait;
+
+    /**
+     * @var WritableStreamInterface
+     */
+    private $stream;
+
+    /**
+     * @var string
+     */
+    private $buffer = '';
+
+    /**
+     * WritableStreamHash constructor.
+     * @param WritableStreamInterface $stream
+     */
+    public function __construct(WritableStreamInterface $stream)
+    {
+        $this->stream = $stream;
+    }
+
+    public function isWritable()
+    {
+        return $this->stream->isWritable();
+    }
+
+    public function write($data)
+    {
+        $this->buffer .= $data;
+
+        return $this->stream->write($this->processBuffer());
+    }
+
+    public function end($data = null)
+    {
+        $this->buffer .= $data;
+        $this->stream->end(
+            $this->processBuffer() . base64_decode($this->buffer)
+        );
+        $this->buffer = '';
+    }
+
+    public function close()
+    {
+        $this->stream->close();
+    }
+
+    private function processBuffer(): string
+    {
+        $length = strlen($this->buffer);
+        $buffer = base64_decode(substr($this->buffer, 0, $length - $length % 4));
+        $this->buffer = substr($this->buffer, $length - $length % 4);
+
+        return $buffer;
+    }
+}
---
+++ tests/WritableStreamBase64DecodeTest.php
@@ -0,0 +1,54 @@
+<?php declare(strict_types=1);
+
+namespace WyriHaximus\React\Tests\Stream\Base64;
+
+use PHPUnit\Framework\TestCase;
+use React\EventLoop\Factory;
+use React\Stream\ThroughStream;
+use WyriHaximus\React\Stream\Base64\WritableStreamBase64Decode;
+use WyriHaximus\React\Stream\Base64\WritableStreamBase64Encode;
+use function Clue\React\Block\await;
+use function React\Promise\Stream\buffer;
+
+final class WritableStreamBase64DecodeTest extends TestCase
+{
+    public function provideData()
+    {
+        yield ['a'];
+        yield ['abc'];
+        yield ['abcdefg'];
+        yield ['abcdefghij'];
+        yield ['abcdefghijklm'];
+        yield ['abcdefghijklmnop'];
+        yield ['abcdefghijklmnopqrst'];
+        yield ['abcdefghijklmnopqrstuvw'];
+        yield ['abcdefghijklmnopqrstuvwxyz'];
+        foreach (range(128, 1337) as $size) {
+            yield [str_pad('a', $size)];
+        }
+        yield [str_pad('a', 1337)];
+        yield [str_pad('a', 100000)];
+        yield [str_pad('a', 1000000)];
+        yield [str_pad('a', 10000000)];
+    }
+
+    /**
+     * @dataProvider provideData
+     */
+    public function testHash(string $data)
+    {
+        $loop = Factory::create();
+        $throughStream = new ThroughStream();
+        $stream = new WritableStreamBase64Decode($throughStream);
+        $loop->futureTick(function () use ($stream, $data) {
+            $data = base64_encode($data);
+            $chunks = str_split($data);
+            $last = count($chunks) - 1;
+            for ($i = 0; $i < $last; $i++) {
+                $stream->write($chunks[$i]);
+            }
+            $stream->end($chunks[$last]);
+        });
+        self::assertSame($data, await(buffer($throughStream), $loop));
+    }
+}
